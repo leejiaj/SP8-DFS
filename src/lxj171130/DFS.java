2e545712a1a007855d5defdf7c637843275c7903
Leejia James <leejiaj@gmail.com>
2018-10-25 18:00:17 -0500

    removed unwanted variables

--- src/lxj171130/DFS.java
+++ src/lxj171130/DFS.java
@@ -24,7 +24,6 @@ public class DFS extends GraphAlgorithm<DFS.DFSVertex> {
 	}
 	int time;
 	int topNum;
-	DFSVertex dfsv;
 	LinkedList<Vertex> finishList;
 	
     public static class DFSVertex implements Factory {
