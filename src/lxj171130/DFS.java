2f900e5a043022e7369cf9cc791fd26381e1bbcf
Leejia James <leejiaj@gmail.com>
2018-10-26 21:07:33 -0500

    implemented dfs and used dfs to get topological ordering

--- src/lxj171130/DFS.java
+++ src/lxj171130/DFS.java
@@ -1,8 +1,12 @@
-/** Starter code for SP8
- *  @author 
+/**
+ * @author Leejia James
+ * @author Nirbhay Sibal
+ *
+ * Comparison of hashing implementations
+ *
+ * Ver 1.0: 2018/10/26
  */
 
-// change to your netid
 package lxj171130;
 
 import rbk.Graph.Vertex;
@@ -25,6 +29,7 @@ public class DFS extends GraphAlgorithm<DFS.DFSVertex> {
 	int time;
 	int topNum;
 	LinkedList<Vertex> finishList;
+	boolean notDAG;
 	
     public static class DFSVertex implements Factory {
 	int cno;
@@ -44,31 +49,38 @@ public class DFS extends GraphAlgorithm<DFS.DFSVertex> {
     public DFS(Graph g) {
 	super(g, new DFSVertex(null));
     }
-
+    
+    /**
+     * Recursive algorithm to visit the nodes of a graph
+     * @param Graph g
+     * @return DFS
+     */
     public static DFS depthFirstSearch(Graph g) {
-	return null;
-    }
-
-    // Member function to find topological order
-    public List<Vertex> topologicalOrder1() throws Exception {
-    	topNum = g.size();
-    	finishList = new LinkedList<>();
+    	DFS dfstemp = new DFS(g);
+    	dfstemp.topNum = g.size();
+    	dfstemp.finishList = new LinkedList<>();
 		for(Vertex u: g) {
-		    get(u).color = Color.WHITE;
-		    get(u).parent = null;
-		    get(u).top = 0;
+		    dfstemp.get(u).color = Color.WHITE;
+		    dfstemp.get(u).parent = null;
+		    dfstemp.get(u).top = 0;
 		}
-		time = 0;
+		dfstemp.time = 0;
 		
     	for(Vertex u: g) {
-    		if(get(u).color == Color.WHITE) {
-    			topologicalOrder1_Visit(u);
+    		if(dfstemp.get(u).color == Color.WHITE) {
+				dfstemp.dfsVisit(u);
     		}
     	}
-	return finishList;
+    	return dfstemp;
     }
 
-    private void topologicalOrder1_Visit(Vertex u) throws Exception {
+    /**
+     * Utility method for depthFirstSearch()
+     * @param Vertex u
+     */
+    private void dfsVisit(Vertex u) { 
+    	// u is visited by DFS
+    	// Precondition: u is white
 		get(u).color = Color.GREY;
 		get(u).dis = ++time;
 		
@@ -76,19 +88,35 @@ public class DFS extends GraphAlgorithm<DFS.DFSVertex> {
 			Vertex v = e.otherEnd(u);
 			if(get(v).color == Color.WHITE) {
 			    get(v).parent = u;
-			    topologicalOrder1_Visit(v);
+			    dfsVisit(v);
 			}
 			else if(get(v).color == Color.GREY) {
-				throw new Exception("Cycles detected, Not a DAG");
+				// cycle detected
+				notDAG = true;
 			}
 	    }
 		get(u).top = topNum--;
 		finishList.addFirst(u);
 		get(u).color = Color.BLACK;
-		get(u).fin = ++time;		
+		get(u).fin = ++time;
 	}
 
-	// Find the number of connected components of the graph g by running dfs.
+	/**
+	 * Member function to find topological order
+	 * @return List of vertices in topological order if graph is DAG, 
+	 * 			null otherwise
+	 */
+    public List<Vertex> topologicalOrder1() {
+    	DFS d1 = depthFirstSearch(g);
+    	if(d1.notDAG) {
+    		return null;
+    	}
+    	else {
+    		return d1.finishList;
+    	}
+    }
+
+    // Find the number of connected components of the graph g by running dfs.
     // Enter the component number of each vertex u in u.cno.
     // Note that the graph g is available as a class field via GraphAlgorithm.
     public int connectedComponents() {
@@ -100,14 +128,14 @@ public class DFS extends GraphAlgorithm<DFS.DFSVertex> {
 	return get(u).cno;
     }
     
-    // Find topological oder of a DAG using DFS. Returns null if g is not a DAG.
+    /**
+     * Finding topological oder of a DAG using DFS
+     * @param Graph g
+     * @return List of vertices in topological order, null if g is not a DAG
+     */
     public static List<Vertex> topologicalOrder1(Graph g) {
-	DFS d = new DFS(g);
-	try {
+		DFS d = new DFS(g);
 		return d.topologicalOrder1();
-	} catch (Exception e) {
-		return null;
-	}
     }
 
     // Find topological oder of a DAG using the second algorithm. Returns null if g is not a DAG.
@@ -117,14 +145,14 @@ public class DFS extends GraphAlgorithm<DFS.DFSVertex> {
 
     public static void main(String[] args) throws Exception {
 	//String string = "7 8   1 2 2   1 3 3   2 4 5   3 4 4   4 5 1   5 1 7   6 7 1   7 6 1 0";
-	String string = "7 6   1 2 2   1 3 3   2 4 5   3 4 4   4 5 1   6 7 1 1";
+	String string = "7 6   1 2 2   1 3 3   2 4 5   3 4 4   5 1 7   7 6 1 0";
 	Scanner in;
 	// If there is a command line argument, use it as file from which
 	// input is read, otherwise use input from string.
 	in = args.length > 0 ? new Scanner(new File(args[0])) : new Scanner(string);
 	
 	// Read graph from input
-       // Graph g = Graph.readGraph(in);
+        //Graph g = Graph.readGraph(in);
         Graph g = Graph.readDirectedGraph(in);
 	g.printGraph(false);
 
@@ -134,11 +162,12 @@ public class DFS extends GraphAlgorithm<DFS.DFSVertex> {
 	for(Vertex u: g) {
 	    System.out.println(u + "\t" + d.cno(u));
 	}
+	System.out.println();
 	
-	List<Vertex> finishList = topologicalOrder1(g);
-	System.out.println("Topological ordering");
+	List<Vertex> finishList = topologicalOrder1(g);	
+	System.out.println("Topological oder of DAG using DFS");
 	if(finishList == null) {
-		System.out.println("Not a DAG");
+		System.out.println("--Cycles detected, Not a DAG--");
 	}
 	else {
 		for(Vertex u: finishList) {
